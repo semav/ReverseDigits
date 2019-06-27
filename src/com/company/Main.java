49b323a82634dac97000d7c2bde5ba9a08e16ac2
Семенов Александр Владимирович <Semenov.Ale.Vla5@sberbank.ru>
2019-06-27 16:32:29 +0300

    Generate prime numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,17 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
     }
 
-    public static int revertDigits(int x){
+    static int revertDigits(int x){
         int result = 0;
         int xRemainning = Math.abs(x);
 
@@ -15,5 +20,36 @@ public class Main {
             xRemainning /= 10;
         }
         return x > 0 ? result : -result;
+    }
+
+    static List<Integer> generatePrimesParallel(int n){
+        return IntStream
+                .range(2, n+1)
+                .parallel()
+                .filter(Main::isPrime)
+                .sorted()
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    public static List<Integer> generatePrimes(int n){
+        List<Integer> result = new ArrayList<>();
+
+        for(int i = 2; i <= n; i++){
+            if (isPrime(i)){
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+
+    private static boolean isPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if ((number % i) == 0 ){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -2,24 +2,43 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class MainTest {
+class MainTest {
     @Test
-    public void revertDigitsTest(){
+    void revertDigitsTest() {
         int result = Main.revertDigits(12345);
-        assertEquals(54321,result);
+        assertEquals(54321, result);
     }
 
     @Test
-    public void revertDigitsTest2(){
+    void revertDigitsTest2() {
         int result = Main.revertDigits(-12345);
-        assertEquals(-54321,result);
+        assertEquals(-54321, result);
     }
 
     @Test
-    public void revertDigitsTest3(){
+    void revertDigitsTest3() {
         int result = Main.revertDigits(112233445);
-        assertEquals(544332211,result);
+        assertEquals(544332211, result);
+    }
+
+    @Test
+    void primeNumbersTest1() {
+        List<Integer> primeNumbers = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+                73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+                179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
+                283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
+                419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541,
+                547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659,
+                661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809);
+
+        List<Integer> result = Main.generatePrimesParallel(809);
+
+        assertEquals(primeNumbers, result);
     }
 }
